40b6fbe7f67c3bc06a9c4b5d30fd460f45e4eca4
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2024-08-22 00:22:53 +0530

    [455] [Time Beats: 48.31%] [Memory Beats: 63.84%] - LeetPush

---
+++ 455-Assign-Cookies.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int ans = 0;
+        sort(g.begin(), g.end()); sort(s.begin(), s.end());
+        for(int i = 0; i < s.size() and ans < g.size(); i++){
+            if(s[i] >= g[ans]) ans++;  
+        }
+        return ans;
+    }
+};
