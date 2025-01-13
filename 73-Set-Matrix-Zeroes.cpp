24b89e367a9a8cdabf0cc9627fd67f8964b0ba33
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-01-13 13:50:15 +0530

    [73] [Time Beats: 100.00%] [Memory Beats: 42.53%] - LeetPush

--- 73-Set-Matrix-Zeroes.cpp
+++ 73-Set-Matrix-Zeroes.cpp
@@ -3,19 +3,24 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> row(m,1), col(n,1);
         for(int i = 0; i < m; i++){
             for(int j = 0; j<n ; j++){
+                if(i == 0 or j == 0){
+                    if(matrix[i][j] == 0){
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
                 if(matrix[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j<n ; j++){
-                    if(row[i] == 0 or col[j] == 0){
+                    if(matrix[i][0] == 0 or matrix[0][j] == 0){
                         matrix[i][j] = 0;
                     }
                 }
