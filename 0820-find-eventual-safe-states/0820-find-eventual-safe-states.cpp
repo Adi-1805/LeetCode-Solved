1b3808466f285d60c4d95b7f304b9c21daf73f26
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-06-19 19:21:05 +0530

    Time: 52 ms (38.37%), Space: 66.4 MB (21.8%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,28 +1,35 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& pathVis, vector<int>& topo){
-        vis[node] = 1; pathVis[node] = 1;
-        for(auto next: adj[node]){
-            if(!vis[next]){
-                if(dfs(next, adj, vis, pathVis, topo) == true) return true; // we found a cycle
-            }else{
-                if(pathVis[next]) return true;
-            }
-        }
-        topo.push_back(node);
-        pathVis[node] = 0;
-
-        return false;
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> vis(n,0);
-        vector<bool> pathVis(n,0);
+        vector<vector<int>> adj(n);
+        for(int i = 0; i < n; i++){
+            for(int node: graph[i]){
+                adj[node].push_back(i);
+            }
+        }
+  
+        vector<int> indegree(n,0);
+        queue<int> q;
         vector<int> topo;
 
         for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                dfs(i, graph, vis, pathVis, topo); // we found a cycle
+            for(int node: adj[i]){
+                indegree[node]++;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto next: adj[node]){
+                indegree[next]--;
+                if(indegree[next] == 0) q.push(next);
             }
         }
 
