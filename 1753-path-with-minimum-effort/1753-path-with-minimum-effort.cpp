334635d472d0f3f902c5e2e7b4c50a6bdcc7710b
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-06-25 15:23:54 +0530

    Time: 135 ms (21.14%), Space: 38.3 MB (25.85%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,34 +1,30 @@
 class Solution {
-    int drow[8] = {-1, 1, 0, 0, -1, -1, 1, 1};
-    int dcol[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+    int drow[4] = {0, 0, -1, 1};
+    int dcol[4] = {-1, 1, 0 ,0};
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int maxAbsDiff = 0;
         int m = heights.size();
         int n = heights[0].size();
-
-        vector<vector<int>> relDiff(m, vector<int>(n, INT_MAX));
-        relDiff[0][0] = heights[0][0];
-
-        priority_queue< pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;   
-        pq.push({0, {0, 0}});
+        vector<vector<int>> effort(m, vector<int>(n, 1e9));
+        effort[0][0] = 0;
+        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > pq;
+        pq.push({0, 0, 0}); // {diff, row, col}
 
         while(!pq.empty()){
-            auto pathEffort = pq.top().first;
-            auto [row, col] = pq.top().second;
+            int diff = pq.top()[0];
+            int row = pq.top()[1];
+            int col = pq.top()[2];
             pq.pop();
-
-            if(row == m-1 and col == n-1) return pathEffort;
+            if(row == m-1 and col == n-1) return diff;
             for(int i = 0; i < 4; i++){
                 int x = row + drow[i];
                 int y = col + dcol[i];
-                if(x >= 0 and x < m and y >= 0 and y < n){
-                    int newEffort = max(pathEffort, abs(heights[x][y] - heights[row][col]));
-                    if(newEffort < relDiff[x][y]){
-                        relDiff[x][y] = newEffort;
-                        pq.push({newEffort, {x, y}});
+                if(x >= 0 and y >=0 and x < m and y < n){
+                    int newEffort = max(diff, abs(heights[row][col]-heights[x][y]));
+                    if(newEffort < effort[x][y]){
+                        effort[x][y] = newEffort;
+                        pq.push({newEffort, x, y});
                     }
-
                 }
             }
         }
