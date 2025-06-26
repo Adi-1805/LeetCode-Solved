f48382dae8fdacafa302a5182619fa016fa35d8b
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-06-27 02:22:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    // RECURSION WITH MEMOIZATION
     int climbStairs(int n) {
-        vector<int> dp(46, -1);
-        dp[0] = 0; dp[1] = 1; dp[2] = 2;
+        if(n < 2) return n;
+        int prev2 = 1, prev = 2, curr = 0;
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
