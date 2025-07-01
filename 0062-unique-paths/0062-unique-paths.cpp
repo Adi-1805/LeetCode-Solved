d05030644162d798afbe16ffa26f36677540d6f3
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-01 16:38:20 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
     vector<vector<int>> dp;
     int solve(int x, int y, int m, int n){
         if(x == m-1 and y == n-1) return 1;
-        if(x > m and y > n) return 0;
+        if(x > m or y > n) return 0;
         if(dp[x][y] != -1) return dp[x][y];
         int down = 0, right = 0;
         if(x+1 < m) down = solve(x+1, y, m, n);
