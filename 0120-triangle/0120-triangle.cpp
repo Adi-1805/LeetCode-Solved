c1d90884dba9adc1318c31b38018f1d2c4f64cae
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-12 02:22:48 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.01%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,19 @@
 class Solution {
-    int n;
-    int solve(int x, int y, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(x < 0 or y < 0 or y > x) return 0;
-        if(x == n-1) return grid[x][y];
-        if(dp[x][y] != -1) return dp[x][y];
-        int down = solve(x+1, y, grid, dp);
-        int downRight = solve(x+1, y+1, grid, dp);
-        return dp[x][y] = grid[x][y] + min(down, downRight);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(0, 0, triangle, dp);
+        int n = triangle.size();
+        vector<int> end(n,0);
+        for(int j=0; j<n; j++) end[j] = triangle[n-1][j];
+
+        for(int row = n-2; row >= 0; row--){
+            vector<int> curr(n, 0);
+            for(int col = row ; col >= 0; col--){
+                int d = triangle[row][col] + end[col];
+                int dg = triangle[row][col] + end[col+1];
+                curr[col] = min(d,dg);
+            }
+            end = curr;
+        }
+	    return end[0];
     }
 };
