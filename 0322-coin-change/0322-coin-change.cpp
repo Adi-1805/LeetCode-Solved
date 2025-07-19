b125a3568fc4e7c916a08a3983e489787dc1aeae
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-19 22:51:01 +0530

    Time: 55 ms (19.43%), Space: 48.8 MB (43.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,31 +1,24 @@
 class Solution {
-    vector<vector<int>> dp;
-    int solve(int ind, int amt, vector<int> &coins){
-        if(ind == 0){
-            if(amt % coins[0] == 0) return amt/coins[0];
-            else return 1e9;
-        }
-        if(dp[ind][amt] != -1) return dp[ind][amt];
-        
-        int not_take = solve(ind-1, amt, coins);
-        int take = 1e9;
-        // since a coin of one denomination can be chosen more than once, we will no change the index when we take
-        if(coins[ind] <= amt) take = 1 + solve(ind, amt - coins[ind], coins);
-        return dp[ind][amt] = min(not_take, take);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        dp.resize(n, vector<int>(amount+1, -1));
-        // bool ans = 1;
-        // for(int i = 0; i < n; i++){
-        //     if(amount%coins[i]) ans = 0;
-        //     else{
-        //         ans = 1; break;
-        //     }
-        // }
-        // if(ans == 0) return -1;
-        int ans = solve(n-1, amount, coins);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        for(int amt = 0; amt <= amount; amt++){
+            if(amt%coins[0]) dp[0][amt] = 1e9;
+            else dp[0][amt] = amt/coins[0];
+        }
+        
+        for(int ind = 1; ind < n; ind++){
+            for(int amt = 0; amt <= amount; amt++){
+                int not_take = dp[ind-1][amt];
+                int take = 1e9;
+                // since a coin of one denomination can be chosen more than once, we will no change the index when we take
+                if(coins[ind] <= amt) take = 1 + dp[ind][amt-coins[ind]];
+                dp[ind][amt] = min(not_take, take);
+            }
+        }
+
+        int ans = dp[n-1][amount];
         return (ans == 1e9) ? -1 : ans;
     }
 };
