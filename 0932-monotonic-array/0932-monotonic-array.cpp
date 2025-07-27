fe24287f71d21335e881e036f5edd5d316a26cd9
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-27 13:47:21 +0530

    Time: 0 ms (100%), Space: 100.3 MB (90.81%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -3,19 +3,19 @@ public:
     bool isMonotonic(vector<int>& arr) {
         int n = arr.size();
         if(n == 1) return 1;
-        bool inc = (arr[1] >= arr[0]) ? 1 : 0; 
-        if(inc){
-            for(int i = 1; i < n; i++){
-                if(arr[i-1] > arr[i]) return 0;
+
+        bool flag1 = 1, flag2 = 1; 
+        for(int i = 1; i < n; i++){
+            if(arr[i-1] > arr[i]){
+                flag1 = 0;
             }
-            return 1;
-        }
-        else{
-            for(int i = 1; i < n; i++){
-                if(arr[i] > arr[i-1]) return 0;
+            else if(arr[i] > arr[i-1]){
+                flag2 = 0;
             }
-            return 1;
+            if(!flag1 and !flag2) break;
         }
+
+        if(flag1 or flag2) return 1;
         return 0;
     }
 };
