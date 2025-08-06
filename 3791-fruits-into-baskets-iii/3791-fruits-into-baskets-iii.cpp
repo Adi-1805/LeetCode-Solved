8dff20b69c8f9fa8b4fd068243b7802d568fa33c
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-06 22:32:28 +0530

    Time: 65 ms (80.5%), Space: 189.2 MB (52.92%) - LeetHub

---
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> segtree;
+    void build(vector<int>& baskets, int i, int l, int r){
+        if(l==r){
+            segtree[i] = baskets[l];
+            return;
+        }
+        int mid = (l+r)/2;
+        build(baskets, 2*i+1, l, mid);
+        build(baskets, 2*i+2, mid+1, r);
+        segtree[i] = max(segtree[2*i+1], segtree[2*i+2]);
+    }
+    bool query(int i, int l, int r, int val){
+        if(segtree[i]<val){
+            return false;
+        }
+        if(l==r){ // reach the leaf node
+            segtree[i] = -1;
+            return true;
+        }
+        int mid = (l+r)/2;
+        bool placed = false;
+        if(segtree[2*i+1]>=val) placed = query(2*i+1, l, mid, val);
+        else placed = query(2*i+2, mid+1, r, val);
+        segtree[i] = max(segtree[2*i+1], segtree[2*i+2]);
+        return placed;
+    }
+    int numOfUnplacedFruits(vector<int>& fruits,  vector<int>& baskets) {
+        // segTree -> 4n
+        int n = fruits.size();
+        segtree.resize(4*n, -1);
+        build(baskets, 0, 0, n-1);//arr, 0, l, r
+        int ans = 0;
+        for(auto x : fruits){
+            if(query(0, 0, n-1, x)==false) ans++;
+        }
+        return ans;
+    }
+};
