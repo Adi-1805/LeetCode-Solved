fe9d3e7eaf9d0067aa1398a041feb6c843c5ad87
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-14 02:13:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.44%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        bitset<32> bits(n);
-        return (bits.count() == 1 and n != INT_MIN);
+        if(n <= 0) return 0;
+        return (n & (n-1)) == 0;
     }
 };
