52794faa7dd2b241f2759a7780aa1a76611ba340
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-15 23:14:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n <= 0) return 0;
-        return (n & (n-1)) == 0 && (n & 0b01010101010101010101010101010101);
+        return (n > 0) && (n & (n-1)) == 0 && (n & 0b01010101010101010101010101010101);
     }
 };
