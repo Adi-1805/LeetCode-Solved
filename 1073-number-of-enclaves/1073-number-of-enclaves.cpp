47216dce4e4d741568b21049c10893e1b9a15243
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-22 16:48:33 +0530

    Time: 8 ms (66.04%), Space: 34.6 MB (52.12%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,42 +1,35 @@
 class Solution {
-    int m, n;
-    int drow[4] = {-1, 0, 1, 0};
-    int dcol[4] = {0, 1, 0, -1};
-    void dfs(vector<vector<int>>& grid, int x, int y, vector<vector<int>>& vis) {
-        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != 1 || vis[x][y]) {
-            return;
-        }
-        vis[x][y] = 1;
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    void dfs(int r, int c, int n, int m, vector<vector<int>>& grid, vector<vector<bool>>& vis){
+        vis[r][c] = 1;
+        grid[r][c] = -1;
         for(int i = 0; i < 4; i++){
-            int r = x + drow[i];
-            int c = y + dcol[i];
-            dfs(grid, r, c, vis);
+            int nr = r + dx[i];
+            int nc = c + dy[i];
+            if(nr >= 0 and nr < n and nc >= 0 and nc < m and !vis[nr][nc] and grid[nr][nc] == 1){
+                dfs(nr, nc, n, m, grid, vis);
+            }
         }
     }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        m = grid.size();
-        if (m == 0) return 0;
-        n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        
-        for (int i = 0; i < m; ++i) {
-            if (grid[i][0] == 1 && !vis[i][0]) dfs(grid, i, 0, vis);
-            if (grid[i][n - 1] == 1 && !vis[i][n - 1]) dfs(grid, i, n - 1, vis);
-        }
-        for (int j = 0; j < n; ++j) {
-            if (grid[0][j] == 1 && !vis[0][j]) dfs(grid, 0, j, vis);
-            if (grid[m - 1][j] == 1 && !vis[m - 1][j]) dfs(grid, m - 1, j, vis);
-        }
-        
-        int answer = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && !vis[i][j]) {
-                    answer++;
-                }
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>> visited(n, vector<bool>(m, 0));
+        // Mark the edge 1s as irreplaceable
+        for(int i = 0; i < n; i++) if(grid[i][0] == 1) dfs(i, 0, n, m, grid, visited);
+        for(int i = 0; i < n; i++) if(grid[i][m-1] == 1) dfs(i, m-1, n, m, grid, visited);
+        for(int j = 0; j < m; j++) if(grid[0][j] == 1) dfs(0, j, n, m, grid, visited);
+        for(int j = 0; j < m; j++) if(grid[n-1][j] == 1) dfs(n-1, j, n, m, grid, visited);
+
+        int landCells = 0;
+        for(int i = 0; i < n; i++){
+            for(int j =0; j < m; j++){
+                if(grid[i][j] == 1) landCells++;
             }
         }
-        return answer;
+        return landCells;
     }
 };
