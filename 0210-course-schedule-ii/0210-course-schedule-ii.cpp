56609ba54ffa34b59036df216a104135de24f792
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-23 15:31:40 +0530

    Time: 2 ms (78.33%), Space: 19.1 MB (13.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,36 +1,46 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& pathVis, vector<int>& topo){
-        vis[node] = 1; pathVis[node] = 1;
-        for(auto next: adj[node]){
-            if(!vis[next]){
-                if(dfs(next, adj, vis, pathVis, topo) == true) return true; // we found a cycle
-            }else{
-                if(pathVis[next]) return true;
+    bool DFScheck(int node, vector<vector<int>>& adjacencyList, vector<int>& visited, vector<int>& pathVisited, stack<int> &revCourseOrder){
+        /* DFScheck function:
+            Traverses graph using recursion and returns true if a cycle is detected in the graph, else return false;
+        */
+        visited[node] = 1;
+        pathVisited[node] = 1;
+        for(int nextNode: adjacencyList[node]){
+            if(!visited[nextNode]){
+                if(DFScheck(nextNode, adjacencyList, visited, pathVisited, revCourseOrder)) return true; //cycle detected
             }
+            else if(pathVisited[nextNode]) return true; // cycle detected
         }
-        topo.push_back(node);
-        pathVis[node] = 0;
 
+        pathVisited[node] = 0;
+        revCourseOrder.push(node);
         return false;
     }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto& vec: prerequisites){
-            adj[vec[1]].push_back(vec[0]); 
+        // Graph simulation of the problem
+        // Each course is a node 
+        if(numCourses == 1) return {0};
+        vector<vector<int>> adjacencyList(numCourses);
+        for(auto pair: prerequisites){
+            adjacencyList[pair[1]].push_back(pair[0]);
         }
 
-        vector<bool> vis(numCourses,0);
-        vector<bool> pathVis(numCourses,0);
-        vector<int> topo;
+        vector<int> visited(numCourses, 0);
+        vector<int> pathVisited(numCourses, 0);
+        stack<int> revCourseOrder;
 
-        for(int i = 0; i < numCourses; i++){
-            if(!vis[i]){
-                if(dfs(i, adj, vis, pathVis, topo) == true) return {}; // we found a cycle
+        for(int course = 0; course < numCourses; course++){
+            if(!visited[course]){
+                if(DFScheck(course, adjacencyList, visited, pathVisited, revCourseOrder)) return {};
             }
         }
 
-        reverse(topo.begin(), topo.end());
-        return topo;
+        vector<int> courseOrder;
+        while(!revCourseOrder.empty()){
+            courseOrder.push_back(revCourseOrder.top());
+            revCourseOrder.pop();
+        }
+        return courseOrder;
     }
 };
