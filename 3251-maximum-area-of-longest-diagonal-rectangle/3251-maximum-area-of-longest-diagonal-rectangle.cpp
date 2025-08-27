d3a0977f57cd1120967e606ed37850e15db7cc93
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-27 15:42:13 +0530

    Time: 0 ms (100%), Space: 29.2 MB (94.39%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -5,10 +5,10 @@ public:
         double longestDiagonal = -1.0;
         for(auto& r: dimensions){
             int x = r[0], y = r[1];
-            if(longestDiagonal < sqrt(x*1.0*x + y*1.0*y)){
+            if(longestDiagonal < (x*1.0*x + y*1.0*y)){
                 maxArea = x*y;
-                longestDiagonal = sqrt(x*1.0*x*1.0 + y*1.0*y*1.0);
-            }else if(longestDiagonal == sqrt(x*1.0*x + y*1.0*y)){
+                longestDiagonal = (x*1.0*x*1.0 + y*1.0*y*1.0);
+            }else if(longestDiagonal == (x*1.0*x + y*1.0*y)){
                 maxArea = max(maxArea, x*y);
             }
         }
