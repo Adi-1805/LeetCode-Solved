149db8d4e621fce2654d646498bd9bf98cbf94af
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-29 23:40:45 +0530

    Time: 58 ms (20.35%), Space: 66.2 MB (50.57%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,26 +1,36 @@
 class Solution {
-    vector<int> topoSort;
-    bool cycleDetectWithDfs(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& pathVis){
-        visited[node] = 1;
-        pathVis[node] = 1;
-        for(int next: graph[node]){
-            if(!visited[next]){
-                if(cycleDetectWithDfs(next, graph, visited, pathVis)) return true;
-            }else if(pathVis[next]){
-                return true;
+    /* KAHN'S ALGORITHM */
+    void KahnAlgo(int V, vector<vector<int>>& adj, vector<int>& topoSort){
+        queue<int> q; 
+        vector<int> indegree(V, 0);
+        for(int i = 0; i < V; i++){
+            for(int verti: adj[i]) indegree[verti]++;
+        }
+        
+        for(int v = 0; v < V; v++) if(indegree[v] == 0) q.push(v);
+        
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            topoSort.push_back(node);
+            for(int next: adj[node]){
+                indegree[next]--;
+                if(indegree[next] == 0) q.push(next);
             }
         }
-        topoSort.push_back(node);
-        pathVis[node] = 0;
-        return false;
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int V = graph.size();
-        vector<bool> visited(V, 0), pathVis(V, 0);
+        vector<int> topoSort;
+        vector<vector<int>> revAdj(V);
         for(int i = 0; i < V; i++){
-            if(!visited[i]) cycleDetectWithDfs(i, graph, visited, pathVis); 
+            for(int verti: graph[i]){
+                revAdj[verti].push_back(i);
+            }
         }
+        
+        KahnAlgo(V, revAdj, topoSort);
         sort(topoSort.begin(), topoSort.end());
         return topoSort;
     }
