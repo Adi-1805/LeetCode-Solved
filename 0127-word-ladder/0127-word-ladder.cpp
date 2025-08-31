1a3653efdb14acce4d9c47b8d339899ec05dafb5
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-31 23:50:20 +0530

    Time: 369 ms (21.8%), Space: 21.3 MB (45.21%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        /*
-         Okay so here you have to return the number of words in the sequence of transformation rather than length of the transformation sequence. Hence if the beginWord is in the list, it will be counted, else not. I will take care of it in the start itself as follows:
-        */
+        set<string> words(wordList.begin(), wordList.end());
 
-        queue<pair<string, int>> q;
-        unordered_set<string> st(wordList.begin(), wordList.end()); 
-        q.push({beginWord, 1});
-        st.erase(beginWord);
-
-        if(st.find(endWord) == st.end()) return 0;
-
-        int answer = 0;
+        int noOfWords = 0;
+        queue<pair<string, int>> q; q.push({beginWord, 1});
         while(!q.empty()){
-            auto [s, len]= q.front();
+            auto [str, len] = q.front();
+            // cout << str << " " << len << endl; 
+            if(str == endWord) return len;
             q.pop();
-            for(int i = 0; i < s.size(); i++){
-                char orig = s[i]; // to trace back
-                for(char ch = 'a'; ch <= 'z'; ch++){
-                    s[i] = ch;
-                    if( st.find(s) != st.end() ){
-                        q.push({s, len+1});
-                        st.erase(s);
+            for(int i = 0; i < str.size(); i++){
+                for(int j = 0; j < 26; j++){
+                    string newS = str;
+                    newS[i] = char('a'+j);
+                    if(words.count(newS) != 0){
+                        q.push({newS, len+1});
+                        words.erase(newS);
                     }
                 }
-                s[i] = orig;
             }
-            if(s == endWord) return len;
-
         }
         return 0;
     }
