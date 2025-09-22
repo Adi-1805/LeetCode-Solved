39eba47865d6dde88d2ab5df830d497ae00f9278
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-09-22 17:58:07 +0530

    Time: 0 ms (100%), Space: 23 MB (89.81%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -3,13 +3,14 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         vector<int> hash(101, 0);
         int maxFreq = 0;
+        int cnt = 0;
         for(int val: nums){
             ++hash[val];
-            if(hash[val] > maxFreq) maxFreq = hash[val];
-        }
-        int cnt = 0;
-        for(int val: hash){
-            if(val == maxFreq) cnt++;
+            if(hash[val] == maxFreq) cnt++;
+            else if(hash[val] > maxFreq){
+                maxFreq = hash[val];
+                cnt = 1;
+            } 
         }
         return cnt*maxFreq;
     }
