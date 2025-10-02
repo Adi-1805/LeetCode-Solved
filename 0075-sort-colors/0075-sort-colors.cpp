b609b6ae3306436abd5d54345ec96332348233b4
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-10-02 18:21:29 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        /* Dutch Flag Algorithm:
+            There are gonna be three pointers, low, mig and high, where we will ensure that everything between low and (mid-1)  is sorted, everything between mid to high is unsorted and everything after high is sorted. 
+            At every point we're trying to achieve:
+            [0, low-1] will contain 0s;
+            [low, mid-1] will contain 1s;
+            [mid, high] will be unsorted;
+            [high+1, n-1] will contain 2s;
+
+        */
+        int n = nums.size();
+        int left = 0, mid = 0, right = n-1;
+        while(mid <= right){
+            if(nums[mid] == 0){
+                swap(nums[mid], nums[left]);
+                mid++, left++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[right]);
+                right--;
+            }
+        }
     }
 };
