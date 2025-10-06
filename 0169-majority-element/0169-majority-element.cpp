4d1ac0f53496949d945f0629d72845e562045640
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-10-06 23:12:14 +0530

    Time: 0 ms (100%), Space: 28.2 MB (63.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // BOYER-MOORE VOTING ALGORITHM
         int n = nums.size();
-        int majorElement = INT_MIN;
-        unordered_map<int, int> mp;
+
+        // Step 1: Find the candidate answer
+        int possibleAnswer = 0;
+        int count = 0;
         for(int i = 0; i < n; i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]] > floor(n/2)) { majorElement = nums[i]; break; }
+            if(count == 0){
+                possibleAnswer = nums[i]; count = 1;
+            }else{
+                if(nums[i] == possibleAnswer) count++;
+                else count--;
+            }
         }
-        return majorElement;
+
+        // Step 2: Check if the candidate is truly majority element
+        count = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == possibleAnswer) count++;   
+        }
+
+        if(count > floor(n/2)) return possibleAnswer;
+
+        return -1;
     }
 };
