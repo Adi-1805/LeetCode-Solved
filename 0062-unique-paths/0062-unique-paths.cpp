48122a5f83915653b4ba050c1b260f4a792d1e71
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-10-07 13:04:46 +0530

    Time: 0 ms (100%), Space: 9.6 MB (9.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,29 +1,23 @@
 class Solution {
-    int f(int m, int n, int row, int col, vector<vector<int>> &dp){
-        if(row < 0 or row >= m or col < 0 or col >= n) return 0;
-        if(row == m-1 and col == n-1) return 1;
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int waysDown = f(m, n, row+1, col, dp);
-        int waysRight = f(m, n, row, col+1, dp);
-
-        return dp[row][col] = waysDown + waysRight;
-    }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> nextRow(n, 0);
 
         for(int row = m-1; row >= 0 ; row--){
+            vector<int> currRow(n, 0);
+
             for(int col = n-1; col >= 0; col--){
-                if(row == m-1 and col == n-1){ dp[row][col] = 1; continue; }
+                if(row == m-1 and col == n-1){ currRow[col] = 1; continue; }
                 
                 int waysDown = 0, waysRight = 0;
-                if(row < m-1) waysDown  = dp[row+1][col];
-                if(col < n-1) waysRight = dp[row][col+1];
+                if(row < m-1) waysDown  = nextRow[col];
+                if(col < n-1) waysRight = currRow[col+1];
 
-                dp[row][col] = waysDown + waysRight;
+                currRow[col] = waysDown + waysRight;
             }
+            nextRow = currRow;
         }
-        return dp[0][0];
+        return nextRow[0];
     }
 };
