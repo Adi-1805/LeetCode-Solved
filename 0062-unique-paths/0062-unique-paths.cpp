62e78f9d6d5274459adc76c944461184c0fc02e5
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-10-07 12:29:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (9.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,32 +1,17 @@
 class Solution {
-    // int solve(int x, int y, int m, int n){
-    //     if(x == m-1 and y == n-1) return 1;
-    //     if(x > m or y > n) return 0;
-    //     if(dp[x][y] != -1) return dp[x][y];
-    //     int down = 0, right = 0;
-    //     if(x+1 < m) down = solve(x+1, y, m, n);
-    //     if(y+1 < n) right = solve(x, y+1, m, n);
-    //     return dp[x][y] = down + right;
-    // }
+    int f(int m, int n, int row, int col, vector<vector<int>> &dp){
+        if(row < 0 or row >= m or col < 0 or col >= n) return 0;
+        if(row == m-1 and col == n-1) return 1;
+        if(dp[row][col] != -1) return dp[row][col];
+
+        int waysDown = f(m, n, row+1, col, dp);
+        int waysRight = f(m, n, row, col+1, dp);
+
+        return dp[row][col] = waysDown + waysRight;
+    }
 public:
     int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp;
-        // dp.resize(m, vector<int>(n, 0));
-        // dp[0][0] = 1;
-        vector<int> prevRow(n, 0);
-        for(int i = 0; i < m; i++){
-            vector<int> temp(n, 0);
-            for(int j = 0; j < n; j++){
-                if(i == 0 and j == 0) temp[j] = 1;
-                else{
-                    int prevUp = 0, prevLeft = 0;
-                    if(i>0) prevUp = prevRow[j];
-                    if(j>0) prevLeft = temp[j-1];
-                    temp[j] = prevUp + prevLeft;
-                }
-            }
-            prevRow = temp;
-        }
-        return prevRow[n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m, n, 0, 0, dp);
     }
 };
