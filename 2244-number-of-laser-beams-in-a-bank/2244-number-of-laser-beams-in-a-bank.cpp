01071d37e75b9aa33b174a5f3b9584c880670e41
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-10-27 12:48:04 +0530

    Time: 7 ms (58.4%), Space: 27.4 MB (68.24%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -3,18 +3,10 @@ public:
     int numberOfBeams(vector<string>& bank) {
         int n = bank.size(), m = bank[0].size();
         long long outBeams = 0, inBeams = 0, answer = 0;
-        int firstFilledRow = 0;
-        // Finding the first filled row with devices
-        while(outBeams == 0 and firstFilledRow < n){
-            bitset<500> bits(bank[firstFilledRow]);
-            outBeams = bits.count();
-            firstFilledRow++;
-        }
-        // Proceeding with the later rows to multiply and propagate forward
-        for(int i = firstFilledRow; i < n; i++){
+        for(int i = 0; i < n; i++){
             bitset<500> bits(bank[i]);
             inBeams = bits.count();
-            if(inBeams != 0 and outBeams != 0){
+            if(inBeams != 0){
                 answer += 1LL*inBeams*outBeams;
                 outBeams = inBeams;
             }
