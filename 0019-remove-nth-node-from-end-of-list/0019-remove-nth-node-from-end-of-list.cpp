3d2e54e958a0e2d69b28bac666c6455843cdb1cd
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-11-02 13:30:06 +0530

    Time: 0 ms (100%), Space: 15 MB (25.28%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,30 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
-    int count;
-    void recur(ListNode* head, int n){
-        if(head == NULL) return;
-        // Traverse to the end of the list first.
-        recur(head -> next, n);
-        // Increment counter as the recursion unwinds.
-        // count = 1 for null, 2 for the last node, etc.
-        count++;
-        // When count reaches n + 1, 'node' is the prev of the target node.
-        if(n+1 == count){
-            ListNode * toDelete = head -> next;
-            head -> next = toDelete -> next;
-        }
-    }
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head == NULL) return NULL;
-        // A temp node allows us to handle edge cases like removing the head.
-        ListNode *temp = new ListNode(0); temp -> next = head;
-        count = 0;
-        recur(temp, n);
-        head = temp -> next;
-        return temp -> next;
+        // (This is my prev failed solution that I corrected)
+        // Adding a dummy node at the beginning of the LL to deal with edge case e.g., deleting the head
+        ListNode *dummy = new ListNode();
+        dummy -> next = head;
+        // temp node decalred to facilitate traversal
+        ListNode* temp = dummy;
+        ListNode *prevToDelete = NULL; // Will try to put this pointer behind the node to be deleted
+        int ind = 0; 
+        while(ind <= n and temp){
+            temp = temp -> next; ind++;
+        }
+        prevToDelete = dummy;
+        // Now the temp and prevToDeleted node have a distance of 'n+1' so when temp reaches end of LL(nullptr) prevToDeleted reaches the node just previous to the node to be deleted
+        while(temp != NULL){
+            temp = temp -> next; 
+            prevToDelete = prevToDelete -> next;
+        }
+        ListNode *toBeDeleted = prevToDelete -> next;
+        prevToDelete -> next = toBeDeleted -> next;
+
+        delete(toBeDeleted);
+        return dummy->next;
     }
 };
