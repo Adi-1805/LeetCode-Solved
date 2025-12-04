10b2c765c4c404ba4662210ab3974283a8d87fd6
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-12-05 00:16:15 +0530

    Time: 3 ms (81.59%), Space: 15.9 MB (18.12%) - LeetHub

---
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int ans = 0, onecnt = 0;
+        for(int i = 1; i < s.size(); i++){
+            if(s[i-1] == '1' and s[i] == '0'){
+                onecnt++;
+                ans += onecnt;
+            }
+            else if(s[i-1] == '1') onecnt++;
+        }
+        // if(s[s.size()-1] == 0) ans += onecnt;
+        return ans;
+    }
+};
