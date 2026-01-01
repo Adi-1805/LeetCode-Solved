ac84a8a0d63a68fb5728777f125e218e92338ac6
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-01-01 21:53:34 +0530

    Time: 0 ms (100%), Space: 11.5 MB (77.72%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> result;
         int n = digits.size();
         int carry = 1;
         for(int i = n-1; i >= 0; i--){
             int digit = digits[i];
-            // cout << digit << " " << carry << endl;
             if(digit + carry >= 10){
-                result.push_back((digit + carry)%10);
+                digits[i] = (digit + carry)%10;
                 carry = 1;
             }else{
-                result.push_back(digit+carry);
+                digits[i] = (digit+carry)%10;
                 carry = 0;
             }
         }
-        if(carry == 1) result.push_back(carry);
-        reverse(result.begin(),result.end());
-        return result;
+        if(carry) digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
