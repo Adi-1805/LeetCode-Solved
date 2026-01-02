bc50ca9e2a155ff3c2f2d1bbb5ed577c125e9ada
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-01-02 20:43:50 +0530

    Time: 0 ms (100%), Space: 28.5 MB (53.6%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -5,6 +5,6 @@ public:
         for(int i = 0; i < n; i++){
             if(nums[i] == nums[(i+1)%n] or nums[i] == nums[(i+2)%n]) return nums[i];
         }
-        return nums[n-1];
+        return -1;
     }
 };
