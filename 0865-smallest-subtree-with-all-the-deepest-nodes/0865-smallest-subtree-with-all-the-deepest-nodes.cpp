23dca99847712364609d6962759391ad8304d37c
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-01-10 01:14:18 +0530

    Time: 0 ms (100%), Space: 16.2 MB (79.92%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int getDepth(TreeNode* root){
+        if(!root) return 0;
+        return 1 + max(getDepth(root->left), getDepth(root->right));
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, int max_depth, int cur_depth){
+        if(!root) return nullptr;
+        if(cur_depth == max_depth) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, max_depth, cur_depth+1);
+        TreeNode* right = lowestCommonAncestor(root->right, max_depth, cur_depth+1);
+        if(left && right) return root;
+        return left ? left : right;
+    }
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int max_depth = getDepth(root);
+        int cur_depth = 1; // root at level 1
+        return lowestCommonAncestor(root, max_depth, cur_depth);
+    }
+};
