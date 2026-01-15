b69382e8a90a59dd1237fd185484fda470abaf7e
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-01-15 21:45:26 +0530

    Time: 6 ms (17.7%), Space: 14.9 MB (21.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int> hash(1001, 0);
+        map<int, int> mp;
         int n = trips.size();
         for(int i = 0; i < n; i++){
             vector<int> trip = trips[i];
-            for(int i = trip[1]; i < trip[2]; i++){
-                hash[i] += trip[0];
-            }
+            mp[trip[1]] += trip[0];
+            mp[trip[2]] -= trip[0];
         }
-        
-        if( capacity >= *max_element(hash.begin(), hash.end()) ) return true;
-        return false;
+
+        int cur_passenger_count = 0;
+        for(auto it: mp){
+            cur_passenger_count += it.second;
+            if(cur_passenger_count > capacity) return false;
+        }
+        return true;
     }
 };
