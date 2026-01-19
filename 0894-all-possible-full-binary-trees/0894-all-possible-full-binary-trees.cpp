61644c6b106a9d759c7309e071335ecffeb6ab02
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-01-19 22:07:36 +0530

    Time: 4 ms (53.48%), Space: 30.8 MB (98.78%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -10,17 +10,20 @@
  * };
  */
 class Solution {
+    unordered_map<int, vector<TreeNode*>> dp; 
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
         vector<TreeNode*> result;
         if((n&1) == 0) return {}; // for even values of n -> no tree possible
         if(n == 1){
-            TreeNode* root = new TreeNode(0);
-            return {root};
+            TreeNode* root = new TreeNode(0); return {root};
         }
+        if(dp.find(n) != dp.end()) return dp[n];
+
         for(int i = 1; i < n-1; i+=2){
             vector<TreeNode*> left = allPossibleFBT(i);
             vector<TreeNode*> right = allPossibleFBT(n-i-1);
+            dp[i] = left, dp[n-i-1] = right;
 
             for(auto L: left){
                 for(auto R: right){
