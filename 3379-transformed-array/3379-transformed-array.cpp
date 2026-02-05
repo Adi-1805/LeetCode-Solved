9ed62f453e97e1a28349a3fea3108b569a55b6d0
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-02-06 00:45:08 +0530

    Time: 12 ms (13.37%), Space: 25.7 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -4,12 +4,8 @@ public:
         int n = nums.size();
         vector<int> result(n);
         for(int i = 0; i < n; i++){
-            if(nums[i] > 0){
-                result[i] = nums[(nums[i]+i)%n];
-            }else if(nums[i] < 0){
-                int offset = abs(nums[i]/n) +1;
-                result[i] = nums[(i+ offset*n +nums[i])%n];
-            }
+            int offset = abs(nums[i]/n) +1;
+            result[i] = nums[(i+ offset*n +nums[i])%n];
         }
         return result;
     }
