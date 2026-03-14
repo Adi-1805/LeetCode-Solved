19218be70e48ea2f05bd4f8cc949d8e770ff1c8c
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-03-15 01:00:08 +0530

    Time: 26 ms (21.24%), Space: 25.7 MB (16.63%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,34 +1,32 @@
 class Solution {
-    void recur(string s, int ind, int n, int k, vector<string>& list){
+    void recur(string s, int ind, int n, vector<string>& list){
         if(ind == n){
-            list.push_back(s);
-            return;
+            list.push_back(s); return;
         }
 
         if(s.empty()){
-            recur(s+'a', ind+1, n, k-1, list);
-            recur(s+'b', ind+1, n, k-1, list);
-            recur(s+'c', ind+1, n, k-1, list);
+            recur(s+'a', ind+1, n, list);
+            recur(s+'b', ind+1, n, list);
+            recur(s+'c', ind+1, n, list);
         }
         else if(s.back() == 'a'){
-            recur(s+'b', ind+1, n, k-1, list);
-            recur(s+'c', ind+1, n, k-1, list);
+            recur(s+'b', ind+1, n, list);
+            recur(s+'c', ind+1, n, list);
         }
         else if(s.back() == 'b'){
-            recur(s+'a', ind+1, n, k-1, list);
-            recur(s+'c', ind+1, n, k-1, list);
+            recur(s+'a', ind+1, n, list);
+            recur(s+'c', ind+1, n, list);
         }
         else if(s.back() == 'c'){
-            recur(s+'a', ind+1, n, k-1, list);
-            recur(s+'b', ind+1, n, k-1, list);
+            recur(s+'a', ind+1, n, list);
+            recur(s+'b', ind+1, n, list);
         }
 
     }
 public:
     string getHappyString(int n, int k) {
         vector<string> list;
-        recur("", 0, n, k, list);
-        for(string s: list) cout << s << " "; cout << endl;
+        recur("", 0, n, list);
         return (list.size() >= k) ? list[k-1] : ""; 
     }
 };
