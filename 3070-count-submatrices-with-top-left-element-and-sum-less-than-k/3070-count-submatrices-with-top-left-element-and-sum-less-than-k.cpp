1f1d0ac80f82824e9d071b144574e7bfef900e41
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-03-19 01:11:57 +0530

    Time: 4 ms (98.33%), Space: 90.6 MB (77.22%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
         int N = grid.size(), M = grid[0].size();
-        vector<vector<int>> pfsum(N+1, vector<int>(M+1, 0));
         int count = 0;
-        for(int i = 1; i <= N; i++){
-            for(int j = 1; j <= M; j++){
-                pfsum[i][j] = grid[i-1][j-1] + pfsum[i-1][j] + pfsum[i][j-1] - pfsum[i-1][j-1];
-                if(pfsum[i][j] <= k) count++;
+        for(int i = 1; i < N; i++) grid[i][0] += grid[i-1][0];
+        for(int j = 1; j < M; j++) grid[0][j] += grid[0][j-1];
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j < M; j++){
+                grid[i][j] = grid[i][j] + grid[i-1][j] + grid[i][j-1] - grid[i-1][j-1];
+            }
+        }
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(grid[i][j] <= k) count++;
             }
         }
         return count;
+        
     }
 };
