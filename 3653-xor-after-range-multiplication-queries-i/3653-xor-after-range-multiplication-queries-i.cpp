faa872c55df07690531c5163706a2246465d8b7f
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-04-09 00:18:02 +0530

    Time: 103 ms (22.97%), Space: 169.3 MB (10.53%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    const int M = 1e9+7;
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        for(auto q : queries){
+            int l = q[0], r = q[1], k = q[2], v = q[3];
+            for(int i = l; i <= r; i += k) nums[i] = (1ll*nums[i]*v)%M;
+        }
+        int res = 0;
+        for(auto x : nums) res ^= x;
+        return res;
+    }
+};
