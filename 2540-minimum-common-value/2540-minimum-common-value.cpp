bbd11522d8ec5e8a98b2d36a155f43f4b43d3b36
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-05-20 00:27:13 +0530

    Time: 474 ms (5.03%), Space: 54.6 MB (58.81%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,12 +1,6 @@
 class Solution {
-    void swap_vectors(vector<int>& nums1, vector<int>& nums2){
-        vector<int> temp = nums1;
-        nums1 = nums2;
-        nums2 = temp;
-    }
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size() < nums1.size()) swap_vectors(nums1, nums2);
         int p1 = 0, p2 = 0;
         while(p1 < nums1.size() and p2 < nums2.size()){ 
             cout << nums1[p1] << " : " << nums2[p2] << endl;
