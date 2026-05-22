c1704c60820dac9e09310767d4fb694f8c5fc817
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-05-22 10:56:40 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.44%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans = 1.0;
+        long long int nn = (n < 0) ? -1LL*n : n;
+        while(nn > 0){
+            if(nn&1) ans = (ans*x);
+            x = (x*x);
+            nn >>= 1;
+        }
+        return (n >= 0) ? ans : (1.0/ans);
     }
 };
