93df6b5e6b9fb9da77a8025000f9d99e022c52d5
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-05-27 01:35:54 +0530

    Time: 0 ms (100%), Space: 36.6 MB (50.22%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,15 +1,22 @@
 class Solution {
-    vector<int> dp;
-    bool f(int ind, vector<int> &arr){
-        if(ind < 0 || ind >= arr.size() || arr[ind] < 0) return 0;
-        if(arr[ind] == 0) return 1;
-        if(dp[ind] != -1) return dp[ind];
-        arr[ind] *= -1; // essential to prevent recursion loops
-        return dp[ind] = ( f(ind-arr[ind], arr) || f(ind+arr[ind], arr) );
-    }
 public:
     bool canReach(vector<int>& arr, int start) {
-        dp.resize(arr.size()+1, -1);
-        return f(start, arr);
+        int n = arr.size();
+        queue<int> q;
+        vector<int> vis(n, 0);
+        q.push(start);
+        vis[start] = 1;
+        while(!q.empty()){
+            int ind = q.front();
+            q.pop();
+            if(arr[ind] == 0) return true;
+            if(ind-arr[ind] >= 0 and !vis[ind-arr[ind]]){
+                q.push(ind-arr[ind]); vis[ind-arr[ind]]=1;
+            }
+            if(ind+arr[ind] < n and !vis[ind+arr[ind]]){
+                q.push(ind+arr[ind]); vis[ind+arr[ind]]=1;
+            }
+        }
+        return false;
     }
 };
