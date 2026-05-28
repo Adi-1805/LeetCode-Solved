48a4068f8ac08d374a6d1712e7b7d78688a24d15
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-05-29 03:57:52 +0530

    Time: 7 ms (92.12%), Space: 19.3 MB (22.13%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    int dfs(int i, vector<int>& arr, int d, vector<int>& dp) {
-        if (dp[i] != -1)
-            return dp[i];
-
-        int best = 1;
-        int n = arr.size();
-
-        for (int nxt = i + 1; nxt <= min(n - 1, i + d); nxt++) {
-            if (arr[nxt] >= arr[i])
-                break;
-
-            best = max(best, 1 + dfs(nxt, arr, d, dp));
+    int dfs(int ind, vector<int>& arr, int d, vector<int>& dp) {
+        if(dp[ind] != -1) return dp[ind];
+        int way1 = 1, way2 = 1;
+        for(int x = 1; x <= d && (ind-x) >= 0; x++){
+            if(arr[ind] > arr[ind-x]){
+                way1 = max(way1, 1+dfs(ind-x, arr, d, dp));
+            }else break;
         }
-
-        for (int nxt = i - 1; nxt >= max(0, i - d); nxt--) {
-            if (arr[nxt] >= arr[i])
-                break;
-
-            best = max(best, 1 + dfs(nxt, arr, d, dp));
+        for(int x = 1; x <= d && (ind+x) < arr.size(); x++){
+            if(arr[ind] > arr[ind+x]){
+                way2 = max(way2, 1+dfs(ind+x, arr, d, dp));
+            }else break;
         }
-
-        return dp[i] = best;
+        return dp[ind] = max(way1, way2);
     }
 
     int maxJumps(vector<int>& arr, int d) {
         int n = arr.size();
         vector<int> dp(n, -1);
-
         int ans = 1;
-
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, dfs(i, arr, d, dp));
-        }
-
+        for(int i = 0; i < n; i++) ans = max(ans, dfs(i, arr, d, dp));
         return ans;
     }
 };
