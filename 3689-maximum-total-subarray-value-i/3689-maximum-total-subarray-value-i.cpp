c8ca8bcef01194e3704ffc0a27b2ac1257122975
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-06-10 02:27:15 +0530

    Time: 0 ms (100%), Space: 103.6 MB (68.6%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        long long mini = LLONG_MAX, maxi = LLONG_MIN;
-        for(int val: nums){
-            if(mini > val) mini = val;
-            if(maxi < val) maxi = val;
-        }
-        return 1LL*k*(maxi-mini);
+        return 1LL * (*max_element(nums.begin(), nums.end()) - *min_element(nums.begin(), nums.end())) * k;
     }
 };
