156060d8b90b21af740229fae6b61692c0475f84
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-07-07 14:53:54 +0530

    Time: 3 ms (42.47%), Space: 15.2 MB (59.74%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -5,15 +5,15 @@ public:
         int overlaps = 0, last = 0;
         for(int i = 1; i < intervals.size(); i++){
             if(intervals[last][0] == intervals[i][0] and intervals[last][1] < intervals[i][1]){
-                cout << "here" << " ";
+                // when intervals overlap and new covers the last interval
                 overlaps++;
                 last = i;
             }else if(intervals[last][1] >= intervals[i][1]){
+                // when intervals overlap and last covers the new interval
                 overlaps++;
             }else{
                 last = i;
             }
-            cout << overlaps << " : " << last << endl;
         }
         return intervals.size() - overlaps;
     }
