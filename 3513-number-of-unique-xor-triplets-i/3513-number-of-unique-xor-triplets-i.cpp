3df2c3983a890fc228dceeb06845bfd14697030d
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-07-24 10:44:04 +0530

    Time: 0 ms (100%), Space: 199.2 MB (9.32%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -3,8 +3,7 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
         if(n <= 2) return n;
-        int ans = 1;
-        while(ans <= n) ans <<= 1;
-        return ans;
+        int bits = floor(log2(n))+1;
+        return pow(2, bits);
     }
 };
