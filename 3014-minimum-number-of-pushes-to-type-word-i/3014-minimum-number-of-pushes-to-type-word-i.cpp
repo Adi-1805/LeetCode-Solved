acea0920558c524da0fabcbf44d5806510ff6c63
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-08-01 00:09:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.71%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,7 +1,10 @@
 class Solution {
-    int LUT[27]={0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 48, 52, 56};
 public:
     int minimumPushes(string word) {
-        return LUT[word.size()];
+        int pushes = 0;
+        for(int i = 0; i < word.size(); i++){
+            pushes += (i/8 + 1);
+        }  
+        return pushes;
     }
 };
